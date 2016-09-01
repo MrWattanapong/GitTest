5fea1669c68192feb986f538a67801620c184526
MrWattanapong <wattanapong.soo@student.mahidol.edu>
2016-09-01 12:09:17 +0700

    change img

--- app/src/main/java/com/hyperx/gittest/MainActivity.java
+++ app/src/main/java/com/hyperx/gittest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hyperx.gittest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    private boolean flag = true;
+    public final static String EXTRA_MESSAGE = "com.egco428.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +34,49 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    }
+
+    public void sendMethod(MenuItem item){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        finish();
+    }
+
+
+    boolean click = true;
+
+    public void Changeimg(MenuItem item){
+    imageView = (ImageView)findViewById(R.id.imageView);
+        if(click){
+            imageView.setImageResource(R.drawable.image2);
+            click = false;
+        }
+
+        else{
+            imageView.setImageResource(R.drawable.image1);
+            click = true;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.add(Menu.NONE, Menu.NONE, 104, "New Toast");
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this,"Hello Toast",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
         return true;
     }
 
